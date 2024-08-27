b4f86955bc52a3d4a9666bb7ff7f9e93e0c23170
vicentemelofilho <vicentemelofilho@gmail.com>
2024-08-27 15:55:59 -0300

    593. Doctests

--- main.py
+++ main.py
@@ -12,3 +12,11 @@ print(soma(10, 20))
 print(soma(-10, 20))
 
 print(soma(10.9, 20.14))
+
+try:
+    print(soma('15', 15))
+except AssertionError as e:    
+    print(f'Conta inválida: {e}')
+# except TypeError as e:
+#     print('Conta inválida')
+#     print(e)
