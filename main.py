da10d43dabb72a872089ee073d8ca86123c80e56
vicentemelofilho <vicentemelofilho@gmail.com>
2024-08-27 14:23:39 -0300

    591. Github da seção

---
+++ main.py
@@ -0,0 +1,14 @@
+#https://docs.python.org/3/library/doctest.html
+#https://docs.python.org/3/library/unittest.html
+
+print('oi')
+
+
+from calculadora import soma
+
+
+print(soma(10, 20))
+
+print(soma(-10, 20))
+
+print(soma(10.9, 20.14))
