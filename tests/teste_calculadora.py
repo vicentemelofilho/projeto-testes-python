baca22774a6a7f39d23afcee7fc08083fd2cb3e9
vicentemelofilho <vicentemelofilho@gmail.com>
2024-08-27 17:52:18 -0300

    597. Unittest #4 - Executando e organizando todos os testes

--- tests/teste_calculadora.py
+++ tests/teste_calculadora.py
@@ -1,3 +1,18 @@
+try:
+    import sys
+    import os
+
+    sys.path.append(
+        os.path.abspath(
+            os.path.join(
+                os.path.dirname(__file__),
+                '../src'
+            )
+        )
+    )
+except:
+    raise
+
 import unittest
 from calculadora import soma
 
